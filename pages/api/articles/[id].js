3bba790e6fee1e99cccdaee6f5532f57a7a2cc81
Fagner Sales <fs.fagnersales@gmail.com>
2021-09-10 20:14:41 -0300

    API Routes

---
+++ pages/api/articles/[id].js
@@ -0,0 +1,12 @@
+import { articles } from '../../../data'
+
+export default function handler({ query }, res) {
+  const { id } = query
+  const filtered = articles.find(article => article.id === id)
+
+  if (filtered) {
+    res.status(200).json(filtered)
+  } else {
+    res.status(404).json({ message: `Article with the id of ${id} is not found` })
+  }
+}
