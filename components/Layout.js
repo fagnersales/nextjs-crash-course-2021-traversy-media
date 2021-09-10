bd255a53090e7b1bea5d48504d0eb8529e5e43e6
Fagner Sales <fs.fagnersales@gmail.com>
2021-09-10 20:37:33 -0300

    Custom Meta component

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,11 @@
+import Meta from './Meta'
 import Nav from './Nav'
 import Header from './Header'
 import styles from '../styles/Layout.module.css'
 
 const Layout = ({ children }) => {
   return (<>
+    <Meta />
     <Nav />
     <div className={styles.container}>
       <main className={styles.main}>
