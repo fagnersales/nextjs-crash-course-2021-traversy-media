468a641ee13b0a37c1a49f9c30b46826216de9f8
Fagner Sales <fs.fagnersales@gmail.com>
2021-09-10 19:44:46 -0300

    getServerSideProps

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,14 +1,31 @@
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 
-const Article = () => {
-  const router = useRouter()
-  const { id } = router.query
+const Article = ({ article }) => {
+  // const router = useRouter()
+  // const { id } = router.query
 
   return (
-    <div>
-      This is article {id}
-    </div>
+    <>
+      <h1>{article.title}</h1>
+      <p>{article.body}</p>
+      <br />
+      <Link href='/'>Go back</Link>
+    </>
   )
+}
+
+export const getServerSideProps = async (context) => {
+  const id = context.params.id
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+
+  const article = await res.json()
+
+  return {
+    props: {
+      article
+    }
+  }
 }
 
 export default Article
