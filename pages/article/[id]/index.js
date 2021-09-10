229a8e933a8485c29aeb8f3983c542d8661ba207
Fagner Sales <fs.fagnersales@gmail.com>
2021-09-10 19:55:43 -0300

    getStaticProps

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
-const Article = ({ article }) => {
+const article = ({ article }) => {
   // const router = useRouter()
   // const { id } = router.query
 
@@ -15,7 +15,7 @@ const Article = ({ article }) => {
   )
 }
 
-export const getServerSideProps = async (context) => {
+export const getStaticProps = async (context) => {
   const id = context.params.id
   const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
 
@@ -28,4 +28,19 @@ export const getServerSideProps = async (context) => {
   }
 }
 
-export default Article
+export const getStaticPaths = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+  const articles = await res.json()
+
+  const ids = articles.map(article => article.id)
+  const paths = ids.map(id => ({
+    params: { id: id.toString() }
+  }))
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export default article
